ffeac09d263758c6f3f4346c813a84edaf2f5d15
Eugene Cathcart <ecthcrt3@outlook.com>
2023-03-14 20:57:55 -0500

    created to do api

---
+++ ToDoAPI/ToDoAPI/Controllers/ToDoesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+using ToDoAPI.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace ToDoAPI.Controllers
+{
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoesController : ControllerBase
+    {
+        private readonly db_a91356_todolistContext _context;
+
+        public ToDoesController(db_a91356_todolistContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDo>>> GetToDos()
+        {
+            var resources = await _context.ToDos.Include("Category").Select(x => new ToDo()
+            {
+                //Assign each resource in our data set to a new Resource Object for this application.
+                ToDoId = x.ToDoId,
+                Name = x.Name,
+                CategoryId = x.CategoryId,
+                Category = x.Category != null ? new Category()
+                {
+                    CategoryId = x.Category.CategoryId,
+                    CatName = x.Category.CatName,
+                    CatDesc = x.Category.CatDesc
+                } : null
+            }).ToListAsync();
+
+            return Ok(resources);
+        }
+
+        // GET: api/ToDoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDo>> GetToDo(int id)
+        {
+            var toDo = await _context.ToDos.Where(x => x.ToDoId == id).Select(x => new ToDo()
+            {
+                //Assign each resource in our data set to a new Resource Object for this application.
+                ToDoId = x.ToDoId,
+                Name = x.Name,
+                CategoryId = x.CategoryId,
+                Category = x.Category != null ? new Category()
+                {
+                    CategoryId = x.Category.CategoryId,
+                    CatName = x.Category.CatName,
+                    CatDesc = x.Category.CatDesc
+                } : null
+            }).FirstOrDefaultAsync();
+
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            return toDo;
+        }
+
+        // PUT: api/ToDoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDo(int id, ToDo toDo)
+        {
+            if (id != toDo.ToDoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ToDoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ToDo>> PostToDo(ToDo toDo)
+        {
+            //if (_context.ToDos == null)
+            //{
+            //    return Problem("Entity set 'db_a91356_todolistContext.ToDos'  is null.");
+            //}
+            //  _context.ToDos.Add(toDo);
+            //  await _context.SaveChangesAsync();
+
+            //  return CreatedAtAction("GetToDo", new { id = toDo.ToDoId }, toDo);
+
+            ToDo newToDo = new ToDo()
+            {
+                Name = toDo.Name,
+                Done = toDo.Done,
+                CategoryId = toDo.CategoryId
+            };
+            _context.ToDos.Add(newToDo);
+            await _context.SaveChangesAsync();
+
+            return Ok(newToDo);
+        }
+
+        // DELETE: api/ToDoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDo(int id)
+        {
+            if (_context.ToDos == null)
+            {
+                return NotFound();
+            }
+            var toDo = await _context.ToDos.FindAsync(id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDos.Remove(toDo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ToDoExists(int id)
+        {
+            return (_context.ToDos?.Any(e => e.ToDoId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ToDoAPI/ToDoAPI/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("OriginPolicy", "http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ToDoAPI.Models.db_a91356_todolistContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoListDB"));
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors();
+
+app.Run();
